2534d12f0e0a60fd43ac42d7d2b2448c50d1b404
TarasDovzhenko <jonnitar03@gmail.com>
2021-10-14 17:54:50 +0300

    DZ2

---
+++ HomeWork #2/main.js
@@ -0,0 +1,53 @@
+let player1 = {
+  name: "Taras",
+  hp: 88,
+  img: "http://reactmarathon-api.herokuapp.com/assets/liukang.gif",
+  weapon: ["axe", "saber", "revolver"],
+  attack() {
+    console.log(player1.name + " fight");
+  },
+};
+
+let player2 = {
+  name: "Petro",
+  hp: 100,
+  img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
+  weapon: ["musket", "shotgun", "bomb"],
+  attack() {
+    console.log(player2.name + " fight");
+  },
+};
+
+function createPlayer(player, namePlayer) {
+  let div1 = document.createElement("div");
+  div1.classList.add(player);
+
+  let div2 = document.createElement("div");
+  div2.classList.add("progressbar");
+
+  let div3 = document.createElement("div");
+  div3.classList.add("life");
+  div3.style.width = namePlayer.hp + "%";
+
+  let div4 = document.createElement("div");
+  div4.classList.add("name");
+  div4.innerText = namePlayer.name;
+
+  let div5 = document.createElement("div");
+  div5.classList.add("character");
+
+  let img = document.createElement("img");
+  img.src = namePlayer.img;
+
+  div2.appendChild(div3);
+  div2.appendChild(div4);
+  div5.appendChild(img);
+  div1.appendChild(div2);
+  div1.appendChild(div5);
+
+  let arenas = document.querySelector(".arenas");
+  arenas.appendChild(div1);
+}
+
+createPlayer("player1", player1);
+createPlayer("player2", player2);
